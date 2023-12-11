ee4a3e69134f803726bed2777533bdf6a7309c52
ray3en <tigranlee@gmail.com>
2023-12-11 19:08:06 +0400

    init

---
+++ PROJECT/frontend/src/App.js
@@ -0,0 +1,28 @@
+import { Link, Route, Routes } from "react-router-dom";
+import ProductListPage from "./pages/ProductListPage";
+
+
+function App() {
+  return (
+    <div >
+      <div style={{display: "flex", gap: '20px'}}>
+        <Link to={'/products/all'}><h2>All products</h2></Link>
+        <Link to={'/products/sales'}><h2>All sales</h2></Link>
+        <Link to={'/category/1'}><h2>Categories 1</h2></Link>
+        <Link to={'/category/2'}><h2>Categories 2</h2></Link>
+        <Link to={'/category/3'}><h2>Categories 3</h2></Link>
+        <Link to={'/category/4'}><h2>Categories 4</h2></Link>
+        <Link to={'/category/5'}><h2>Categories 5</h2></Link>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/products/all" element={<ProductListPage type='all'/>}/>
+          <Route path="/products/sales" element={<ProductListPage type='sale'/>}/>
+          <Route path="/category/:id" element={<ProductListPage type='categories'/>}/>
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ PROJECT/frontend/src/store/index.js
@@ -0,0 +1,8 @@
+import {applyMiddleware, combineReducers, createStore} from 'redux'
+import {thunk} from 'redux-thunk'
+import { productListReducer } from './productListReducer'
+
+const rootReducer = combineReducers({
+    productList: productListReducer
+})
+export const store = createStore(rootReducer, applyMiddleware(thunk))
