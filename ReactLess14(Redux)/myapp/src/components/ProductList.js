051c643b47ba2f7c8aebbf4824407614d6ca5b18
ray3en <tigranlee@gmail.com>
2023-12-18 20:21:23 +0400

    18.12

--- ReactLess14(Redux)/myapp/src/components/ProductList.js
+++ ReactLess14(Redux)/myapp/src/components/ProductList.js
@@ -1,16 +1,16 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchALlProducts } from "../asyncActions/products"
-import { filterBySaleAction } from "../store/ProductListReducer"
+import { filterByPriceAction, filterBySaleAction } from "../store/ProductListReducer"
 
 
 
 function ProductList(){
 
-    const productList = useSelector(store => store.productList).filter(elem => elem.isShow)
+    const productList = useSelector(store => store.productList).filter(elem => elem.isShowSale && elem.isShowPrice)
     const dispatch = useDispatch()
 
-    console.log(productList)
+    // console.log(productList)
 
     useEffect(() => {
         dispatch(fetchALlProducts())
@@ -20,16 +20,28 @@ function ProductList(){
         dispatch(filterBySaleAction(e.target.checked))
     }
 
+    function formHandler(e){
+        let form_data = new FormData(e.target.parentElement)
+        let data = Object.fromEntries(form_data)
+        data.min = (data.min) ? +data.min : 0
+        data.max = (data.max && (+data.max >= data.min)) ? +data.max : Infinity
+        dispatch(filterByPriceAction(data))
+    }
+
     return(
         <div>
             <div>
+                <form onChange={formHandler}>
+                    <input name='min' placeholder="ОТ" type="number"/>
+                    <input name="max" placeholder='ДО' type="number"/>
+                </form>
                 <label> Sale:
                     <input onClick={checkboxHandle} type="checkbox"/>
                 </label>
             </div>
             <div>
                 {productList.map(elem => 
-                    <div>
+                    <div key={elem.id}>
                         <h3>{elem.title}</h3>
                         <p style={{color: (elem.discountPercentage) ? 'red' : 'black'}}>
                             {elem.price}
--- ReactLess14(Redux)/myapp/src/store/ProductListReducer.js
+++ ReactLess14(Redux)/myapp/src/store/ProductListReducer.js
@@ -3,27 +3,45 @@ const defaultState = []
 
 const ADD_PRODUCTS = 'ADD_PRODUCTS'
 const FILTER_BY_SALE = 'FILTER_BY_SALE'
+const FILTER_BY_PRICE = 'FILTER_BY_PRICE'
 
 export const productListReducer = (state = defaultState, action) => {
     switch(action.type){
         case ADD_PRODUCTS:
             let changed_products_by_fetch = action.payload.map(elem => {
-                elem.isShow = true
+                elem.isShowSale = true
+                elem.isShowPrice = true
                 elem.discountPercentage = (elem.discountPercentage < 10) ? null : elem.discountPercentage
                 return elem
             })
             return changed_products_by_fetch
         case FILTER_BY_SALE:
             let changed_products_by_sale = state.map(elem => {
-                elem.isShow = (!action.payload) ? true : Boolean(elem.discountPercentage)
+                elem.isShowSale = (!action.payload) ? true : Boolean(elem.discountPercentage)
                 return elem
             })
             return changed_products_by_sale
+        case FILTER_BY_PRICE:
+            let {max, min} = action.payload
+            let changed_products_by_price = state.map(elem => {
+                elem.isShowPrice = true
+                if (!(elem.price >= min && elem.price <= max)){
+                    elem.isShowPrice = false
+                }
+                return elem
+            })
+            return changed_products_by_price
         default:
             return state
     }
 }
 
 
+let checkbox = ('sale') ? true : action.payload.checkbox
+
+(msx > price && min < price && checkbox)
+
+
 export const addProductsAction = (payload) => ({type: ADD_PRODUCTS, payload})
 export const filterBySaleAction = (payload) => ({type: FILTER_BY_SALE, payload})
+export const filterByPriceAction = (payload) => ({type: FILTER_BY_PRICE, payload})
